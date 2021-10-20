56a4002f289bf64acbce0a6b10bd0967bc713507
Wanchaloem Laoket <wanchaloem.laokeut@googlemail.com>
2021-10-20 20:41:26 +0700

    update main

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,6 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api');
-
   //  เพิ่ม Swagger UI => OpenAPI
   const config = new DocumentBuilder()
     .setTitle('Tranning Nestjs')
